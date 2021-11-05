9b88f3a6bc33e54c0e013dc85d0eb10ac093a442
Sebastian Duda <git@sebdu.de>
2021-11-06 00:08:21 +0100

    Implement Reboot

--- DeskCtrl.ino
+++ DeskCtrl.ino
@@ -1,13 +1,19 @@
 #include "distance.h"
 #include "wifi.h"
 
+void reboot() {
+  Serial.println("Rebooting...");
+  delay(5000);
+  ESP.restart();
+}
+
 void setup() {
   Serial.begin(115200);
   delay (100);
   Serial.println();
   Serial.println();
   Serial.flush();
-  
+
   Serial.println("Booting...");
   Serial.println(" Initializing distance sensor");
   switch (init_distance()) {
@@ -15,23 +21,23 @@ void setup() {
       break;
     default:
       Serial.println(" Error initializing distance sensor");
-      while (1){}
+      reboot();
   }
   Serial.println(" Distance sensor initialized");
-  
+
   Serial.print(" Connecting Wifi...");
-  switch(init_wifi ()) {
+  switch (init_wifi ()) {
     case 0:
       break;
     default:
       Serial.println(" Error initializing wifi");
-      while (1){}
+      reboot();
   }
   Serial.println(" Wifi connected!");
   Serial.println("Booted!");
 }
 
 void loop() {
-  Serial.print("Distance: ");Serial.print(getHeight());Serial.print("cm; Qualtiy: ");Serial.println(getSensorQuality());
+  Serial.print("Distance: "); Serial.print(getHeight()); Serial.print("cm; Qualtiy: "); Serial.println(getSensorQuality());
   delay(1000);
 }
